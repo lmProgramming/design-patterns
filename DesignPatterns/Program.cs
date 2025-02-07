053c122a78a860987cb8cde7934d33ac77937cf4
jantar <mikikubs@gmail.com>
2025-02-07 19:32:31 +0100

    first commit

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,11 @@
+﻿using DesignPatterns;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var singleton = new Singleton();
+
+        Singleton.Instance.value = 5;
+    }
+}
---
+++ DesignPatterns/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public sealed class Singleton
+    {
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                }
+                return _instance;
+            }
+        }
+
+        private static Singleton _instance;
+        public float value;
+    }
+}
