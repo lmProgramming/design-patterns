9c56916f2c75a6bddbaf58887c75a37593293665
jantar <mikikubs@gmail.com>
2025-02-07 20:58:57 +0100

    feat: domain model with tests

---
+++ DesignPatterns/Layer/DomainModel.cs
@@ -0,0 +1,39 @@
+﻿namespace DesignPatterns.Layer
+{
+    public class DopeAccount : Account
+    {
+        public DopeAccount(int _id, decimal _balance) : base(_id, _balance)
+        {
+        }
+
+        public void Debit(decimal amount, out bool success)
+        {
+            if (!HasEnoughBalance(amount))
+            {
+                success = false;
+                return;
+            }
+
+            Balance -= amount;
+            success = true;
+        }
+
+        public bool HasEnoughBalance(decimal amount)
+        {
+            return Balance > amount;
+        }
+
+        public void TransferTo(Account accountTo, decimal amount, out bool success)
+        {
+            if (!HasEnoughBalance(amount))
+            {
+                success = false;
+                return;
+            }
+
+            Balance -= amount;
+            accountTo.Balance += amount;
+            success = true;
+        }
+    }
+}
--- Tests/LayerTests.cs
+++ Tests/LayerTests.cs
@@ -6,7 +6,7 @@ namespace Tests;
 public class LayerTests
 {
     [Fact]
-    public void Test1()
+    public void TranscriptionScriptTest()
     {
         var accountDao = new AccountDao();
         accountDao.CreateBaseData();
@@ -25,5 +25,34 @@ public class LayerTests
         Assert.Equal(250, account2.Balance);
 
         Assert.Throws<ArgumentException>(() => service.Transfer(2, 1, 300));
+    }
+
+    [Fact]
+    public void DomainModelTest()
+    {
+        var accountDao = new AccountDao();
+        accountDao.CreateBaseData();
+
+        var newAccount = new DopeAccount(3, 500);
+        var account1 = accountDao.GetAccount(1)!;
+        var account2 = accountDao.GetAccount(2)!;
+
+        accountDao.AddAccount(newAccount);
+
+        Assert.Equal(100, account1.Balance);
+        Assert.Equal(200, account2.Balance);
+        Assert.Equal(500, newAccount.Balance);
+
+        newAccount.TransferTo(account2, 50, out bool success);
+
+        Assert.Equal(450, newAccount.Balance);
+        Assert.Equal(250, account2.Balance);
+        Assert.True(success);
+
+        newAccount.TransferTo(account1, 10000, out success);
+
+        Assert.Equal(450, newAccount.Balance);
+        Assert.Equal(100, account1.Balance);
+        Assert.False(success);
     }
 }
