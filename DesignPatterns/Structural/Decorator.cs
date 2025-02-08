c4f0759943ed977060e366ecebbfeb559dd1e5a4
jantar <mikikubs@gmail.com>
2025-02-08 11:55:58 +0100

    feat: added decorator with tests

---
+++ DesignPatterns/Structural/Decorator.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+
+namespace DesignPatterns.Structural;
+
+public abstract class Component
+{
+    public abstract string Operation();
+}
+
+public class UserInfo : Component
+{
+    public override string Operation()
+    {
+        return "mikołaj kubś, pwr";
+    }
+}
+
+public abstract class Decorator(Component component) : Component
+{
+    public Component? Component { get; set; } = component;
+
+    public override string Operation()
+    {
+        if (Component is not null)
+        {
+            return Component.Operation();
+        }
+        
+        return string.Empty;
+    }
+}
+
+public class Capitalizer(Component component) : Decorator(component)
+{
+    private readonly TextInfo _textInfo = new CultureInfo("en-US", false).TextInfo;
+    private readonly Component _component = component;
+
+    public override string Operation()
+    {
+        return _textInfo.ToTitleCase(_component.Operation());
+    }
+}
+
+public class Introductor(Component component) : Decorator(component)
+{
+    public override string Operation()
+    {
+        return $"Yep, that's me: {base.Operation()}";
+    }
+}
---
+++ Tests/StructuralTests.cs
@@ -0,0 +1,23 @@
+﻿using DesignPatterns;
+using DesignPatterns.Structural;
+
+namespace Tests;
+
+public class StructuralTests
+{
+    [Fact]
+    public void DecoratorTests()
+    {
+        var simple = new UserInfo();
+        
+        Assert.Equal("mikołaj kubś, pwr", simple.Operation());
+
+        var decoratedA = new Capitalizer(simple);
+        
+        Assert.Equal("Mikołaj Kubś, Pwr", decoratedA.Operation());
+        
+        var decoratedB = new Introductor(decoratedA);
+        
+        Assert.Equal("Yep, that's me: Mikołaj Kubś, Pwr", decoratedB.Operation());
+    }
+}
