f6370bd10f02ed8fb4c590be908ab65fc1a65019
jantar <mikikubs@gmail.com>
2025-02-08 14:09:01 +0100

    feat: added abstract factory with tests

---
+++ DesignPatterns/Creation/AbstractFactory.cs
@@ -0,0 +1,70 @@
+﻿namespace DesignPatterns.Creation;
+
+public abstract class AbstractFactory
+{
+    public abstract A CreateA();
+
+    public abstract B CreateB();
+}
+
+public abstract class A(string text)
+{
+    public string Hello()
+    {
+        return $"My text is: {text}";
+    }
+}
+
+public abstract class B(int number)
+{
+    public int MyNumber()
+    {
+        return number;
+    }
+}
+
+public class A1(string text) : A(text.ToLower())
+{
+    
+}
+
+public class A2(string text) : A(text.ToUpper())
+{
+    
+}
+
+public class B1(int number) : B(number * 10)
+{
+    
+}
+
+public class B2(int number) : B(-number)
+{
+    
+}
+
+public class ConcreteFactoryA : AbstractFactory
+{
+    public override A CreateA()
+    {
+        return new A1("Hello");
+    }
+
+    public override B CreateB()
+    {
+        return new B1(10);
+    }
+}
+
+public class ConcreteFactoryB : AbstractFactory
+{
+    public override A CreateA()
+    {
+        return new A2("Hello");
+    }
+
+    public override B CreateB()
+    {
+        return new B2(10);
+    }
+}
--- Tests/CreationTests.cs
+++ Tests/CreationTests.cs
@@ -5,10 +5,30 @@ namespace Tests;
 public class CreationTests
 {
     [Fact]
-    public void Test1()
+    public void SingletonTests()
     {
         Singleton.Instance.value = 1;
 
         Assert.Equal(1, Singleton.Instance.value);
+    }
+
+    [Fact]
+    public void AbstractFactoryTests()
+    {
+        var factory = new ConcreteFactoryA();
+        
+        var a = factory.CreateA();
+        var b = factory.CreateB();
+        
+        Assert.Equal("My text is: hello", a.Hello());
+        Assert.Equal(100, b.MyNumber());
+        
+        var factory2 = new ConcreteFactoryB();
+        
+        a = factory2.CreateA();
+        b = factory2.CreateB();
+        
+        Assert.Equal("My text is: HELLO", a.Hello());
+        Assert.Equal(-10, b.MyNumber());
     }
 }
